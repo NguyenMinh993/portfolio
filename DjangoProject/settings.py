9d8a3a6b9a2374c7134bb0951a9add22a306b03b
NguyenMinh <nguyenminh090903@gmail.com>
2025-11-19 23:19:18 +0700

    Remove Azure Storage, use WhiteNoise only for Render

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio',
-    'storages', # Add django-storages
 ]
 
 MIDDLEWARE = [
@@ -132,22 +131,12 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = []
 
-# WhiteNoise configuration for serving static files
+# WhiteNoise configuration for serving static files (always use WhiteNoise)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Azure Storage Settings (optional - only if you want to use Azure)
-if not DEBUG and os.environ.get('AZURE_ACCOUNT_NAME'):
-    AZURE_STORAGE_ACCOUNT_NAME = os.environ.get('AZURE_ACCOUNT_NAME')
-    AZURE_STORAGE_ACCOUNT_KEY = os.environ.get('AZURE_ACCOUNT_KEY')
-    AZURE_CONTAINER_STATIC = os.environ.get('AZURE_CONTAINER_STATIC', 'static')
-    AZURE_CONTAINER_MEDIA = os.environ.get('AZURE_CONTAINER_MEDIA', 'media')
-
-    DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
-
-    AZURE_CUSTOM_DOMAIN = f'{AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net'
-    MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER_MEDIA}/'
-
-    AZURE_URL_EXPIRATION_SECS = 3600 # Optional: URL expiration for private blobs
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
